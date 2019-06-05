ac7d5b43e00bf55dafd91aa8ab7b1b1e930fc6a6
Doncle85 <50951934+Doncle85@users.noreply.github.com>
2019-06-05 15:58:14 +0200

    Add files via upload

---
+++ grosbobo.js
@@ -0,0 +1,56 @@
+var epaule = document.getElementById('epaule');
+
+epaule.onmouseover = function() {
+    document.getElementById('text-epaule').style.display = "block";
+}
+
+epaule.onmouseout = function() {
+    document.getElementById('text-epaule').style.display = "none";
+}
+
+var poignet = document.getElementById('poignet');
+
+poignet.onmouseover = function() {
+    document.getElementById('text-poignet').style.display = "block";
+}
+
+poignet.onmouseout = function() {
+    document.getElementById('text-poignet').style.display = "none";
+}
+
+var coude = document.getElementById('coude');
+
+coude.onmouseover = function() {
+    document.getElementById('text-coude').style.display = "block";
+}
+
+coude.onmouseout = function() {
+    document.getElementById('text-coude').style.display = "none";
+}
+
+var genoux = document.getElementById('genoux');
+
+genoux.onmouseover = function() {
+    document.getElementById('text-genoux').style.display = "block";
+}
+
+genoux.onmouseout = function() {
+    document.getElementById('text-genoux').style.display = "none";
+}
+
+var cheville = document.getElementById('chevilles');
+
+cheville.onmouseover = function() {
+    document.getElementById('text-cheville').style.display = "block";
+}
+
+cheville.onmouseout = function() {
+    document.getElementById('text-cheville').style.display = "none";
+}
+
+
+
+
+
+
+
